b4440da2b7074889178823539ac30ded79343fda
Jeff Trull <edaskel@att.net>
2020-08-06 19:56:05 -0700

    Require packages I depend on

--- tspew.el
+++ tspew.el
@@ -15,6 +15,9 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+(require 'compile)
+(require 'cc-mode)
+
 (defgroup tspew nil
   "Display C++ compilation results more cleanly.
 Suggested usage: (add-hook 'compilation-mode-hook 'tspew-mode)
